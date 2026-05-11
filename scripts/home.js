590d9019a728ebc4428bc6c8b59c140caf7ef7b5
rafael_colin <rafael_colin@estudante.sesisenai.org.br>
2026-05-11 12:25:16 -0300

    add - adicionei a funcionalidade de troca de aba de todos os botões da navbar lateral

---
+++ scripts/home.js
@@ -0,0 +1,15 @@
+function telaSensores() {
+    window.location.href = "sensores.html";
+}
+function telaHome(){
+    window.location.href = "home.html";
+}
+function telaTrens(){
+    window.location.href = "trem.html";
+}
+function telaRelatorios(){
+window.location.href = "relatorios.html";
+}
+function telaSair(){
+    window.location.href = "../index.html";
+}
