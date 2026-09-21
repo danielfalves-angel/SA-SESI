5404af8560a3a80ceb6af3c012b19ed3b77dee53
rafael_colin <rafael_colin@estudante.sesisenai.org.br>
2026-09-21 10:14:04 -0300

    ref - cancel

--- infra/connect.php
+++ infra/connect.php
@@ -4,7 +4,7 @@
     $user = "root";
     $password = "";
     $database = "orbital_trens";
-    $conn = mysqli_connect($host, $user, $password, $database);
+    $conn = mysqli_connect($host, $user, $password, $database, 3307);
 
     if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
